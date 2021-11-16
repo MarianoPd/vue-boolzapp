b02fc943fee89a08698f5414f9bfe063ed53d6ee
MarianoPd <marianopidone.com>
2021-11-16 12:34:50 +0100

    Migliorata struttura dati

--- js/script.js
+++ js/script.js
@@ -2,82 +2,114 @@ var app = new Vue({
     el: '#app',
 
     data: {
-      contacts: [
-          {
-              name: 'Michele',
-              avatar: '_1',
-              visibile: true,
-              messages: [
-                  {
-                      date: '10/01/2020 15:30:55',
-                      text: 'Hai portato a spasso il cane?',
-                      status: 'sent'
-                  },
-                  {
-                      date: '10/01/2020 15:50:00',
-                      text: 'Ricordati di dargli da mangiare',
-                      status: 'sent'
-                  },
-                  {
-                      date: '10/01/2020 16:15:22',
-                      text: 'Tutto fatto?',
-                      status: 'received'
-                  },
-              ],
-          },
-          {
-              name: 'Michele',
-              avatar: '_2',
-              visibile: true,
-              messages: [
-                  {
-                      date: '10/01/2020 15:30:55',
-                      text: 'Hai portato a spasso il cane?',
-                      status: 'sent'
-                  },
-                  {
-                      date: '10/01/2020 15:50:00',
-                      text: 'Ricordati di dargli da mangiare',
-                      status: 'sent'
-                  },
-                  {
-                      date: '10/01/2020 16:15:22',
-                      text: 'Tutto fatto?',
-                      status: 'received'
-                  },
-              ],
-          },
-          {
-              name: 'Michele',
-              avatar: '_3',
-              visibile: true,
-              messages: [
-                  {
-                      date: '10/01/2020 15:30:55',
-                      text: 'Hai portato a spasso il cane?',
-                      status: 'sent'
-                  },
-                  {
-                      date: '10/01/2020 15:50:00',
-                      text: 'Ricordati di dargli da mangiare',
-                      status: 'sent'
-                  },
-                  {
-                      date: '10/01/2020 16:15:22',
-                      text: 'Tutto fatto?',
-                      status: 'received'
-                  },
-              ],
-          },
-      ],
-      //sostituire con -1 quando sviluppato meglio
-      activeMessage: 0,
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+                visible: true,
+                messages: [ 
+                    {
+                        date: '20/03/2020 16:30:00',
+                        message: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        message: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:37:00',
+                        message: 'Ma allora cosa me lo dici a fare "Ci vediamo?',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        message: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        message: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        message: 'Ah scusa!',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 17:15:22',
+                        message: 'Dille che mi porta due lattughe',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_6',// sostituito perché Luisa con la barba non si poteva veder
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:55:00',
+                        message: 'Non sei mai contenta! Statti a casa!',
+                        status: 'sent'
+                    },
+                ],
+            },
+        ],
+        //sostituire con -1 quando sviluppato meglio
+        activeContact: 0,
 
     },
 
 
     methods:{
-        setActive(contactIndex){ this.activeMessage = contactIndex},
+        setActive(contactIndex){ this.activeContact = contactIndex},
         
     },
   })
